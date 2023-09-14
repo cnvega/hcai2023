042114958cbff7b9ea0bb2eb874fbf7f28871885
Cristian Vega Martínez <cristian.vegam@userena.cl>
2023-09-14 15:45:52 -0300

    Clase 14/sept

---
+++ Codes/2.3/myhola.c
@@ -0,0 +1,16 @@
+#include "myhola.h"
+
+
+int saludotriste() 
+{
+   printf("Hola mundo, soy un estudiante de postgrado :(\n"); 
+   return 0; 
+}
+
+void mostrar_array()
+{
+   int i;
+   for(i=0; i<20; i++)
+      printf("%d ", i);
+   printf("\n");
+}
---
+++ Codes/2.3/programa.c
@@ -0,0 +1,8 @@
+#include "myhola.h"
+
+// Esta es la funcion principal de mi programa
+void main()
+{
+   saludotriste();
+   mostrar_array();
+}
