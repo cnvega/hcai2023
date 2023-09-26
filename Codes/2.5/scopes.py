12175d068f1227924b0c4097b78b1a9b2b310261
Cristian A. Vega Martínez <cristian.vegam@userena.cl>
2023-09-26 14:04:26 -0300

    Fix y ejemplo clase 2.5

---
+++ Codes/2.5/scopes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+## Ejemplo para comprender visibilidad de namespaces (scopes)
+
+def scope_test():
+   def do_local():
+      spam = "local spam"
+
+   def do_nonlocal():
+      nonlocal spam
+      spam = "nonlocal spam"
+
+   def do_global():
+      global spam
+      spam = "global spam"
+
+   spam = "test spam"
+   do_local()
+   print("After local assignment:", spam)
+   do_nonlocal()
+   print("After nonlocal assignment:", spam)
+   do_global()
+   print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
